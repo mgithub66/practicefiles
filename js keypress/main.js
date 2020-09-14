52fef184032228cf6c41ed6fceab9708950ae0d7
mohamadreza karimi <m.karimipost@gmail.com>
2020-09-14 13:31:10 +0430

    add all files to repo

---
+++ js keypress/main.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+window.addEventListener('keydown', e => {
+  let k=e.keyCode,audio,audiopath;
+  
+  switch(k){
+  case 87:
+  audiopath = './audio/w.wav'
+      break;
+  case 65:
+  audiopath = './audio/a.wav'
+      break;
+  case 83:
+  audiopath = './audio/s.wav'
+        break;
+  case 68:
+  audiopath = './audio/d.wav'
+        break; 
+   case 76:
+   audiopath = './audio/l.wav'
+        break;
+  case 75:
+  audiopath = './audio/k.wav'
+       break;               
+  case 78:
+  audiopath = './audio/n.wav'   
+  }
+  audio = new Audio(audiopath);
+  audio.play();
+  
+})
